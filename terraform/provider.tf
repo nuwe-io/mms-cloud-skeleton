22193db5f75b8e2302676928d811bd5e14c899f5
Sergi Asensio <sergi.asensio@hitachivantara.com>
2023-03-28 00:59:46 +0200

    remove unused provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,12 +5,12 @@ provider "google" {
   region  = var.region
 }
 
-provider "kubectl" {
-  load_config_file       = false
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-}
+# provider "kubectl" {
+#   load_config_file       = false
+#   host                   = "https://${google_container_cluster.primary.endpoint}"
+#   token                  = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+# }
 
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
